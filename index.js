d5063a0f4b760965ef1b2965ed90c02af24f1db6
David Bahr <dbahr88@gmail.com>
2017-11-01 23:49:47 -0400

    parsed json arrays and file readers added

---
+++ index.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+const fileOne = require("./people1.json");
+const fileTwo = require("./people2.json");
+const completeFile = 'peopleComplete.txt';
+
+function megamultifunction(fileOne, fileTwo) {
+	fs.readFile(fileOne, 'utf-8', function(err, data1) {
+		if (err) throw err;
+		var firstFile = JSON.parse(data1);
+
+		fs.readFile(fileTwo, 'utf-8', function(err, data2) {
+  		if (err) throw err;
+  		var secondFile = JSON.parse(data2);
+  		var merge = firstFile.concat(secondFile);
+  		console.log(merge.sort());
+
+megamultifunction("./people1.json", "./people2.json");
